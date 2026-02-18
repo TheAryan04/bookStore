0a96013dee69cd058c47d4577f66b8a18e82986c
Aryan Mehta <aaryanmehta04@gmail.com>
2026-02-19 02:58:24 +0530

    feat: add cron job to make periodic GET requests to API

---
+++ backend/src/lib/cron.js
@@ -0,0 +1,13 @@
+import cron from "cron";
+import https from "https";
+
+const job = new cron.CronJob("*/14 * * * *", () => {
+    https
+        .get(process.env.API_URL, (res) => {
+            if (res.statusCode === 200) console.log("GET request successfully");
+            else console.log("GET request failed with status code:", res.statusCode);
+        })
+        .on("error", (e) => console.error("Error making GET request:", e));
+});
+
+export default job;
