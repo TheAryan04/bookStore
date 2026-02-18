301797dae7b73bb07bb59fdb1e5c3d20df74c5e7
Aryan Mehta <aaryanmehta04@gmail.com>
2026-02-19 03:08:03 +0530

    chore: add explanatory comments for cron job scheduling in cron.js

--- backend/src/lib/cron.js
+++ backend/src/lib/cron.js
@@ -11,3 +11,18 @@ const job = new cron.CronJob("*/14 * * * *", () => {
 });
 
 export default job;
+
+// CRON JOB EXPLANATION:
+// Cron jobs are scheduled tasks that run periodically at fixed intervals
+// we want to send 1 GET request for every 14 minutes
+
+// How to define a "Schedule"?
+// You define a schedule using a cron expression, which consists of five fields representing:
+
+//! MINUTE, HOUR, DAY OF MONTH, MONTH, and DAY OF WEEK
+//? EXAMPLES && EXPLANATION:
+//* 14 * * * * - Every 14 minutes
+//* 0 0 * * 0 - At midnight on every Sunday
+//* 30 3 15 * * - At 3:30 AM, on the 15th of every month
+//* 0 0 1 1 * - At midnight on January 1st every year
+//* 0 * * * * - At the start of every hour
