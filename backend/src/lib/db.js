f2ca95fa73b17f26903db32bbd56754c79a58929
TheAryan <aaryanmehta04@gmail.com>
2026-06-14 11:08:48 +0530

    fix: simplify MongoDB connection log message in db.js

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
     try {
         const connect = await mongoose.connect(process.env.MONGODB_URI);
-        console.log(`MongoDB connected ${connect.connection.host}`);
+        console.log('MongoDB connected');
     } catch (error) {
         console.error("MongoDB connection error:", error);
         process.exit(1);
